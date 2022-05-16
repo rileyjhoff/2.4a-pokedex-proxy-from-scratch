12991f052ceddf221367ef13d4482740843e9bf8
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-16 14:26:29 -0700

    wrote api fetch function. wrote load() function in App. now, pokemon data is fetched from the pokedex api on load. pokemon state is set equal to the pokemon data that is fetched. map through the array of pokemon state and render an h1 tag with the pokemon's name. validated that the repo is connected to the pokedex endpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import { getPokemon } from './services/fetch-utils';
 
 function App() {
-  const [pokemon, getPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
   const [search, setSearch] = useState('');
 
-  async function load(){}
+  async function load() {
+    const pokemon = await getPokemon();
+    setPokemon(pokemon.data.results);
+  }
 
   useEffect(() => {
     load();
   }, [search]);
 
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
     load();
     setSearch('');
   }
 
-  return <div className="App">
-
-  </div>;
+  return (
+    <div className="App">
+      {pokemon.map((pokemon) => (
+        <div key={pokemon._id}>
+          <h1>{pokemon.pokemon}</h1>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,1 +1,6 @@
-export async function getPokemon(name) {}
+export async function getPokemon() {
+  const rawResponse = await fetch(`/.netlify/functions/pokemon`);
+  const data = await rawResponse.json();
+
+  return data;
+}

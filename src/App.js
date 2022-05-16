2d4d5d01d922679980c5fb783a9582b2e9efe950
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-16 14:10:31 -0700

    initial file setup and code outline in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemon, getPokemon] = useState([]);
+  const [search, setSearch] = useState('');
+
+  async function load(){}
+
+  useEffect(() => {
+    load();
+  }, [search]);
+
+  handleSubmit(e) {
+    e.preventDefault();
+    load();
+    setSearch('');
+  }
+
+  return <div className="App">
+
+  </div>;
 }
 
 export default App;

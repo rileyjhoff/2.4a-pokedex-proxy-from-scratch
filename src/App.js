e630c4d8d5e246e32943d7a4e6b130b44d5890e7
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-16 14:31:18 -0700

    added basic loading spinner functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import { getPokemon } from './services/fetch-utils';
+import LoadingSpinner from './components/LoadingSpinner';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function load() {
+    setIsLoading(true);
     const pokemon = await getPokemon();
     setPokemon(pokemon.data.results);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -23,6 +27,7 @@ function App() {
 
   return (
     <div className="App">
+      {isLoading && <LoadingSpinner />}
       {pokemon.map((pokemon) => (
         <div key={pokemon._id}>
           <h1>{pokemon.pokemon}</h1>
